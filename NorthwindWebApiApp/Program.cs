49438a93e28352d2095089c79f7b30c8bf1f2838
Shenn2948 <shenn2948@gmail.com>
2020-01-02 21:37:49 +0300

    Add serilog.

--- NorthwindWebApiApp/Program.cs
+++ NorthwindWebApiApp/Program.cs
@@ -1,6 +1,11 @@
-﻿using Microsoft.AspNetCore.Hosting;
+﻿using System;
+
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+
+using Serilog;
+using Serilog.Events;
 
 namespace NorthwindWebApiApp
 {
@@ -8,7 +13,25 @@ namespace NorthwindWebApiApp
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+                .Enrich.FromLogContext()
+                .WriteTo.Console().CreateLogger();
+
+            try
+            {
+                Log.Information("Starting web host");
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Host terminated unexpectedly");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
@@ -24,6 +47,7 @@ namespace NorthwindWebApiApp
                         config.ClearProviders();
                         config.AddConsole();
                         config.AddDebug();
-                    });
+                    })
+                .UseSerilog();
     }
 }
