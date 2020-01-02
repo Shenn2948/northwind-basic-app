8ae75b9a9029441ca60014a51fd59a5ca41ba249
Shenn2948 <shenn2948@gmail.com>
2020-01-02 19:49:44 +0300

    Add versioning for OrdersController.

--- NorthwindWebApiApp/Controllers/OrdersController.cs
+++ NorthwindWebApiApp/Controllers/OrdersController.cs
@@ -1,33 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+
 using Microsoft.AspNetCore.Mvc;
+
 using NorthwindWebApiApp.Models;
 using NorthwindWebApiApp.Services;
 
 namespace NorthwindWebApiApp.Controllers
 {
-  [Route("api/[controller]")]
-  [ApiController]
-  public class OrdersController : ControllerBase
-  {
-    private readonly IOrderService orderService;
-
-    public OrdersController(IOrderService orderService)
+    [ApiController]
+    [ApiVersion("1.0")]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    public class OrdersController : ControllerBase
     {
-      this.orderService = orderService ?? throw new ArgumentNullException(nameof(orderService));
-    }
+        private readonly IOrderService orderService;
 
-    [HttpGet]
-    public async Task<ActionResult<IEnumerable<BriefOrderModel>>> GetOrders()
-    {
-      return this.Ok(await this.orderService.GetOrdersAsync());
-    }
+        public OrdersController(IOrderService orderService)
+        {
+            this.orderService = orderService ?? throw new ArgumentNullException(nameof(orderService));
+        }
 
-    [HttpGet("{orderId}")]
-    public async Task<ActionResult<FullOrderModel>> GetOrder(int orderId)
-    {
-      return this.Ok(await this.orderService.GetOrderAsync(orderId));
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<BriefOrderModel>>> GetOrders()
+        {
+            return this.Ok(await this.orderService.GetOrdersAsync());
+        }
+
+        [HttpGet("{orderId}")]
+        public async Task<ActionResult<FullOrderModel>> GetOrder(int orderId)
+        {
+            return this.Ok(await this.orderService.GetOrderAsync(orderId));
+        }
     }
-  }
 }
